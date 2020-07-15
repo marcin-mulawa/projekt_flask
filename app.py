a51880c428576c6a856344526a13caeaa5852377
Maciej Wąsiatycz <56920706+macwascdv@users.noreply.github.com>
2020-07-15 21:28:23 +0200

    mkdir update

--- app.py
+++ app.py
@@ -9,5 +9,8 @@ def index():
 if __name__ == '__main__':
 
     db.create_all()
-    
+    try:
+        os.mkdir(f'myproject/datasets/saved')
+    except Exception as e:
+        pass
     app.run(debug=True)
